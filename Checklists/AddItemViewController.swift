c70786f667e71a006af8690be52780ab22e0501c
Quyet Vu <quyetvv_55@vnu.edu.vn>
2018-07-15 14:06:12 +0700

    add item screen

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
     }
 
     @IBAction func cancel() {
@@ -21,6 +29,20 @@ class AddItemViewController: UITableViewController {
     }
     
     @IBAction func done() {
+        print("Content of text field: \(textField.text!)")
         navigationController?.popViewController(animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
     }
 }
